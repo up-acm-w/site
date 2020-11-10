33f04e31330699d99e8077acc15c742a3a7165a4
sarahiaguilar <sarahiaguilargonzalez@gmail.com>
2020-11-10 01:14:13 -0600

    Updating next event

--- assets/js/main.js
+++ assets/js/main.js
@@ -221,7 +221,7 @@
 })(jQuery);
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 30, 2020 16:00:00").getTime();
+var countDownDate = new Date("Nov 12, 2020 15:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
